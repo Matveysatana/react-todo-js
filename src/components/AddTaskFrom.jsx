64c2c9da3ffce646c7a161eee33a82af4bb9c62a
Motty <satanovskijmatvej8@gmail.com>
2025-11-04 07:46:59 +0300

    Декомпозиция верстки на компоненты

---
+++ src/components/AddTaskFrom.jsx
@@ -0,0 +1,14 @@
+import Button from "./Button";
+import Field from "./Field";
+
+
+const AddTaskFrom = () => {
+  return (
+    <form className="todo__form">
+      <Field />
+      <Button />
+    </form>
+  );
+};
+
+export default AddTaskFrom;
